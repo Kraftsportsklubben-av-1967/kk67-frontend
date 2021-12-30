d06922197267b3801c92e60b07f47bea75d458f0
Marius Arhaug <mariusarhaug@hotmail.com>
2021-12-30 01:40:38 +0100

    FEAT: add navbar and header components
    (still needs queries for mobile sizes) (#1)

---
+++ src/components/NavbarButton.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="text-sm font-bold p-4">
+    <button
+      v-if="focus"
+      class="
+        bg-white
+        border-8
+        rounded-md
+        border-transparent
+        text-black
+        hover:bg-red-600 hover:text-slate-50
+      "
+    >
+      <strong>{{ title }}</strong>
+    </button>
+    <button
+      v-else
+      class="
+        border-8
+        bg-transparent
+        rounded-md
+        border-transparent
+        text-white
+        hover:bg-white hover:text-black
+      "
+    >
+      <strong>{{ title }}</strong>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export interface INavbarButton {
+  title: string
+  url: string
+  focus?: boolean
+}
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    focus: {
+      type: Boolean,
+    },
+  },
+})
+</script>
+<style scoped>
+button {
+  transition-duration: 0.4s;
+}
+</style>
