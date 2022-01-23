84c3ae419556e7ed073742e429f896f174a5dc48
MariusArhaug <mariusarhaug@hotmail.com>
2022-01-23 02:37:30 +0100

    FEAT: add support for localserver proxy on different urls

--- src/constants/contstants.ts
+++ src/constants/contstants.ts
@@ -1,1 +1,2 @@
 export const PRODUCTION = import.meta.env.NODE_DEV === 'production'
+export const HOSTNAME = window.location.hostname
--- src/views/Meets/constants.ts
+++ src/views/Meets/constants.ts
@@ -1,6 +1,6 @@
-import { PRODUCTION } from '../../constants/'
+import { PRODUCTION, HOSTNAME } from '../../constants/'
 
-export const PROXY_URL = PRODUCTION ? 'https://styrkeloft.no/' : 'http://localhost:8010/proxy/'
+export const PROXY_URL = PRODUCTION ? 'https://styrkeloft.no/' : `http://${HOSTNAME}:8010/proxy`
 
-export const PREVIOUS_MEETS = `${PROXY_URL}resultatservice_integrasjon/rss_protokoll.php?m=1&klubb_id=13`
-export const UPCOMING_MEETS = `${PROXY_URL}stevner/?page=ical&skipHeader=1&&k=13`
+export const PREVIOUS_MEETS = `${PROXY_URL}/resultatservice_integrasjon/rss_protokoll.php?m=1&klubb_id=13`
+export const UPCOMING_MEETS = `${PROXY_URL}/stevner/?page=ical&skipHeader=1&&k=13`
