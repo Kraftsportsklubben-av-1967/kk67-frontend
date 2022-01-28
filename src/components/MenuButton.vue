49abc6263f25370666892f384879571f9e84b53f
Marius Arhaug <mariusarhaug@hotmail.com>
2022-01-28 15:42:23 +0100

    FEAT: add about us info (#16)
    * FEAT: add about us info
    
    * DRAFT: add google maps api deps (#17)
    
    * DRAFT: add google maps api deps
    
    * FEAT: add about routes
    
    * FEAT: add google maps support and info
    
    * CHANGE: fixed yarn config for windows
    
    * FEAT: Menu is working, May want slight alteration to styling
    
    * Fixed typo in About.vue
    
    * CHANGE: refactor some misc logic
    
    Co-authored-by: KnutEinar <knut.einar.skaar@gmail.com>

---
+++ src/components/MenuButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="router-link">
+    <router-link :to="path">
+      <p class="font-medium text-md pb-4">
+        <span v-if="focus" class="text-red-600 pr-2">&#11044;</span>
+        {{ title }}
+      </p>
+    </router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export interface IMenuButton {
+  title: string
+  path: string
+  focus: boolean
+}
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    path: {
+      type: String,
+      required: true,
+    },
+    focus: {
+      type: Boolean,
+      required: true,
+    },
+  },
+})
+</script>
+<style scoped lang="scss">
+.router-link:hover {
+  @apply text-gray-500;
+
+  span {
+    @apply text-red-500;
+  }
+}
+</style>
