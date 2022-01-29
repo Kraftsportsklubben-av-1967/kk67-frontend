fe26d4840d3868aae0688c10ddcc6b596c1b12eb
MariusArhaug <mariusarhaug@hotmail.com>
2022-01-30 00:58:05 +0100

    FEAT: add additional breakpoints to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
         'darker-gray': '#080808',
       },
     },
+    screens: {
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [],
 }
