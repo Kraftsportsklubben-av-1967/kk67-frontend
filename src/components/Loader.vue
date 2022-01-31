38bc7a86117cec4f4920a62e75c31214e3c1ef04
Marius Arhaug <mariusarhaug@hotmail.com>
2022-02-01 00:03:27 +0100

    FEAT: loader to upcoming meets (#37)
    * FEAT: loader to upcoming meets
    
    * FEAT: removed faulty files
    
    * FEAT: faulty files exterminated 100%
    
    * CHANGE: fix if loading logic
    
    * FEAT: add extra hover effect to menu
    
    author: yololars <larsengoemonsen@gmail.com>

---
+++ src/components/Loader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="border-solid border-t-red-600 border-gray-200 rounded-full w-48 h-48 flex justify-center loader"
+  />
+</template>
+
+<style scoped>
+.loader {
+  border-width: 7.5px;
+  animation: spin 1.5s linear infinite;
+  margin: auto;
+}
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
