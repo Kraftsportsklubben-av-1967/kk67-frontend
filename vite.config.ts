121597075939a3583c4fee82470f3935fd7a043e
MariusArhaug <mariusarhaug@hotmail.com>
2022-04-28 16:38:29 +0200

    CHANGE: update public folder

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ const pathResolve = (newPath: string) => path.resolve(__dirname, newPath)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  root: './',
+  build: {
+    outDir: 'dist',
+  },
+  publicDir: 'assets',
   resolve: {
     alias: {
       '@': pathResolve('./src'),
