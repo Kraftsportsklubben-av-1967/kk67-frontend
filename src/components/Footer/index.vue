900c9848ede071aa76e8314f1cbfe57c9b3472f2
MariusArhaug <mariusarhaug@hotmail.com>
2022-08-26 12:55:23 +0200

    add 2xl boundaries

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -1,7 +1,9 @@
 <template>
   <footer class="border-red-600 border-t-4 h-fit text-white flex flex-col pt-10 bg-black">
-    <FooterInfo class="mx-8" />
-    <FooterContact class="mx-8 mb-12" />
+    <div class="main-container">
+      <FooterInfo class="mx-8" />
+      <FooterContact class="mx-8 mb-12" />
+    </div>
   </footer>
 </template>
 
