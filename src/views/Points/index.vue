1ca8b5dcb878cb0efe897ba8be62c11add4f8e08
Marius Arhaug <mariusarhaug@hotmail.com>
2024-12-20 22:48:32 +0100

    Feat/add points calculator (#94)
    * Add points calculator
    
    * Add license

---
+++ src/views/Points/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="lg:flex lg:mt-0 lg:justify-evenly w-full">
+        <Calc class="mt-10 lg:mt-0" />
+        <Calc class="mt-10 lg:mt-0"/>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Calc from './Calc.vue'
+
+export default defineComponent({
+    name: 'Points',
+    components: {
+        Calc,
+    },
+  })
+
+
+</script>
+  
